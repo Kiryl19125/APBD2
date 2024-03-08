d2a437cbcff6cabdc857dac79320898334fafacf
kiryl <zhydalovich.kiryl@gmail.com>
2024-03-08 20:41:39 +0100

    init GetAvg function

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,7 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
+{
+    Console.WriteLine(GetAvg([1, 2, 3, 4, 5]));
+}
 
 // modification 1
 // modification 2
 // modification 3
+
+static double GetAvg(int[] arr)
+{
+    return 0;
+}
