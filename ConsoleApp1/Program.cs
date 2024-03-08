f762cd613ec100eed0afd54fbaf8a6fe98772f6d
kiryl <zhydalovich.kiryl@gmail.com>
2024-03-08 20:44:12 +0100

    Finished GetAvg function

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,15 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World!");
-{
-    Console.WriteLine(GetAvg([1, 2, 3, 4, 5]));
-}
+Console.WriteLine(GetAvg([1, 2, 3, 4, 5]));
+
+return;
 
 // modification 1
 // modification 2
 // modification 3
 
-static double GetAvg(int[] arr)
+static float GetAvg(int[] arr)
 {
-    return 0;
+    int sum = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        sum += arr[i];
+    }
+
+    return (float)sum / arr.Length;
 }
